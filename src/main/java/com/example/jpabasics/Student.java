48af350bd947bd10977a030d223635b5c015dbe8
rohit <rohitme0091@gmail.com>
2023-05-12 17:05:56 +0530

    first commit

---
+++ src/main/java/com/example/jpabasics/Student.java
@@ -0,0 +1,26 @@
+package com.example.jpabasics;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    private int studentId;
+
+    private int age;
+
+    private String name;
+
+    private int marks;
+
+}
